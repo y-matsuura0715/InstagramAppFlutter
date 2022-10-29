b8a7750c1380cb87118e1b5fa22baef77a4ecf76
y-matsuura0715 <y-matsuura0715@outlook.jp>
2022-10-29 21:07:27 +0900

    Setup/Theming the App & Building Responsive Layout Widget

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+
+  const ResponsiveLayout({
+    Key? key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        if (constrains.maxWidth > webScreenSize) {
+          return webScreenLayout;
+          //Web screen
+        }
+        return mobileScreenLayout;
+        //mobile screen
+      },
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromARGB(0, 0, 0, 1);
+const webBackgroundColor = Color.fromARGB(18, 18, 18, 1);
+const mobileSearchColor = Color.fromARGB(18, 18, 18, 1);
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const primaryColor = Colors.white;
